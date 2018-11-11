db16a8a63c646eca4b77f21a7ad3389c0cc0e3e2
lavbox <gupta.lav@gmail.com>
2018-11-11 23:52:15 +0000

    Adding TimeZoneConverter library

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TimeZoneConverter;
+using System.Linq;
+
+namespace timezonetest
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine(string.Format("{0,-40}{1,-45}{2,-45}","Id","DisplayName","StandardName"));
+            Console.WriteLine(string.Format("{0,-40}{1,-45}{2,-45}","__","___________","____________"));
+            Console.WriteLine("\n");
+            foreach(var item in TimeZoneInfo.GetSystemTimeZones())
+		    {	
+                Console.WriteLine(string.Format("{0,-40}{1,-45}{2,-45}",item.Id,item.DisplayName,item.StandardName));
+		    }	
+            Console.WriteLine(string.Format("{0,-40}{1,-45}{2,-45}","__","___________","____________"));
+            Console.WriteLine("\n");
+        }
+
+        static void Main(string[] args)
+        {
+           TimeZoneInfo info = null;
+            foreach(var item in TimeZoneInfo.GetSystemTimeZones())
+            {   
+               if(TZConvert.TryGetTimeZoneInfo(item.DisplayName, out info))
+               {
+                   Console.WriteLine(item.DisplayName);
+               }
+            }
+           
+        }
+    }
+}
