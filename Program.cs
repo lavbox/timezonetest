e339899edf7d16b4029881ea539380dd51a3a392
Lav Kumar Gupta <lav.kumar@appliedis.com>
2018-11-12 06:04:37 +0530

    list standard timezone

--- Program.cs
+++ Program.cs
@@ -1,35 +1,56 @@
 ﻿using System;
 using TimeZoneConverter;
 using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace timezonetest
 {
     class Program
     {
-        static void Main1(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine(string.Format("{0,-40}{1,-45}{2,-45}","Id","DisplayName","StandardName"));
-            Console.WriteLine(string.Format("{0,-40}{1,-45}{2,-45}","__","___________","____________"));
+            string environment = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "Windows" : "Linux";
+            var content = new StringBuilder("");
+            var names = new Dictionary<string, string>();
+            foreach (var item in TimeZoneInfo.GetSystemTimeZones().OrderBy(p=>p.StandardName))
+            {
+                if (!names.ContainsKey(item.StandardName))
+                {
+                    names.Add(item.StandardName, "");
+                    content.Append(item.StandardName);
+                    content.Append(Environment.NewLine);
+                }
+            }
+            Console.Write(content.ToString());
+            System.IO.File.WriteAllText(string.Format("../../../{0}.StandardName.txt",environment), content.ToString());
+        }
+
+        static void Main2(string[] args)
+        {
+            Console.WriteLine(string.Format("{0,-40}{1,-45}{2,-45}", "Id", "DisplayName", "StandardName"));
+            Console.WriteLine(string.Format("{0,-40}{1,-45}{2,-45}", "__", "___________", "____________"));
             Console.WriteLine("\n");
-            foreach(var item in TimeZoneInfo.GetSystemTimeZones())
-		    {	
-                Console.WriteLine(string.Format("{0,-40}{1,-45}{2,-45}",item.Id,item.DisplayName,item.StandardName));
-		    }	
-            Console.WriteLine(string.Format("{0,-40}{1,-45}{2,-45}","__","___________","____________"));
+            foreach (var item in TimeZoneInfo.GetSystemTimeZones())
+            {
+                Console.WriteLine(string.Format("{0,-40}{1,-45}{2,-45}", item.Id, item.DisplayName, item.StandardName));
+            }
+            Console.WriteLine(string.Format("{0,-40}{1,-45}{2,-45}", "__", "___________", "____________"));
             Console.WriteLine("\n");
         }
 
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
-           TimeZoneInfo info = null;
-            foreach(var item in TimeZoneInfo.GetSystemTimeZones())
-            {   
-               if(TZConvert.TryGetTimeZoneInfo(item.DisplayName, out info))
-               {
-                   Console.WriteLine(item.DisplayName);
-               }
+            TimeZoneInfo info = null;
+            foreach (var item in TimeZoneInfo.GetSystemTimeZones())
+            {
+                if (TZConvert.TryGetTimeZoneInfo(item.DisplayName, out info))
+                {
+                    Console.WriteLine(item.DisplayName);
+                }
             }
-           
+
         }
     }
 }
